bca2a95c286285d1456b313341030f476435292d
LUCY <wangecikariu.luc@gmail.com>
2023-09-09 12:13:57 +0300

    State Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [onCart, setOnCart] = useState(true);
+
+  function handleCart ( ) {
+    setOnCart((onCart) => !onCart);
+  }
+
+const addOnCart = onCart ? "Add to Cart" : "Remove From Cart";
   return (
-    <li className="">
+      <li className={onCart? "" : "in-cart"}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={addOnCart} onClick={handleCart}>{onCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
